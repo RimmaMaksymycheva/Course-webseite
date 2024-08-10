6bdc904606f7765ff018828fd65d276b207af88c
RimmaMaksymycheva <157027670+RimmaMaksymycheva@users.noreply.github.com>
2024-08-10 12:59:24 +0200

    translation documents

---
+++ de.js
@@ -0,0 +1,22 @@
+if (!window.data) {
+    window.data = {}; // Initialize data if not already defined
+}
+
+window.data.de = {
+    "title": "Startseite",
+    "1": "Über uns",
+    "2": "Kurse",
+    "3": "Team",
+    "4": "Spenden",
+    "5": "Kontakt",
+    "6": "Sprache: ",
+    "Hi": "Hallo",
+    "7": "Teilnehmer",
+    "Kontakt": "Kontakt",
+    "name": "Name",
+    "surname": "Nachname",
+    "email": "E-Mail",
+    "tel": "Telefonnummer",
+    "topic": "Thema",
+    "message": "Nachricht"
+};
---
+++ en.js
@@ -0,0 +1,22 @@
+if (!window.data) {
+    window.data = {}; // Initialize data if not already defined
+}
+
+window.data.en = {
+    "title": "Home Page",
+    "1": "About us",
+    "2": "Courses",
+    "3": "Team",
+    "4": "Donate",
+    "5": "Contact",
+    "6": "Language: ",
+    "Hi": "Hello",
+    "7": "Course members",
+    "Kontakt": "Contact",
+    "name": "Name",
+    "surname": "Surname",
+    "email": "Email",
+    "tel": "Phone number",
+    "topic": "Topic",
+    "message": "Message"
+};
